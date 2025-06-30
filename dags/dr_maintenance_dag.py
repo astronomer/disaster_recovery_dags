67ba7c0f0977471fd13071e6692e8c98a551b86c
jonathanleek <jonathanleek@gmail.com>
2025-06-30 15:13:11 -0500

    Functions for retrieving environment information

--- dags/dr_maintenance_dag.py
+++ dags/dr_maintenance_dag.py
@@ -1,55 +1,62 @@
-from datetime import datetime
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.models.xcom_arg import XComArg
-from include.get_deployments import get_deployments
-from include.create_backup_workspaces import create_backup_workspaces
-from include.create_backup_deployments import create_backup_deployments
-from include.manage_backup_hibernation import manage_backup_hibernation
-
-default_args = {
-    "owner": "airflow",
-    "retries": 3,
-    "retry_delay": 60,
-}
-
-with DAG(
-    dag_id="dr_maintenance_dag",
-    start_date=datetime(2023, 1, 1),
-    schedule_interval=None,
-    catchup=False,
-    default_args=default_args,
-    description="Disaster recovery: create backup workspaces from primary deployments",
-) as dag:
-
-    get_deployments_task = PythonOperator(
-        task_id="get_deployments",
-        python_callable=get_deployments,
-        op_kwargs={"mode": "source"},
-    )
-
-    create_backup_workspaces_task = PythonOperator(
-        task_id="create_backup_workspaces",
-        python_callable=create_backup_workspaces,
-        op_args=[XComArg(get_deployments_task)],
-    )
-
-    create_backup_deployments_task = PythonOperator(
-        task_id="create_backup_deployments",
-        python_callable=create_backup_deployments,
-    )
-
-    unhibernate_backup_deployments_task = PythonOperator(
-    task_id="unhibernate_backup_deployments",
-    python_callable=manage_backup_hibernation,
-    op_kwargs={"deployment_set": "backup", "action": "unhibernate"},
-    )
-
-    hibernate_backup_deployments_task = PythonOperator(
-    task_id="hibernate_backup_deployments",
-    python_callable=manage_backup_hibernation,
-    op_kwargs={"deployment_set": "backup", "action": "hibernate"},
-    )
-
-    # Set task dependencies
-    get_deployments_task >> create_backup_workspaces_task >> create_backup_deployments_task >> unhibernate_backup_deployments_task >> hibernate_backup_deployments_task
+# from datetime import datetime
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
+# from airflow.models.xcom_arg import XComArg
+# from include import assign_tokens_to_backups
+# from include.get_deployments import get_deployments
+# from include.create_backup_workspaces import create_backup_workspaces
+# from include.create_backup_deployments import create_backup_deployments
+# from include.manage_backup_hibernation import manage_backup_hibernation
+# from include.assign_tokens_to_backups import sync_tokens_to_backups
+#
+# default_args = {
+#     "owner": "airflow",
+#     "retries": 3,
+#     "retry_delay": 60,
+# }
+#
+# with DAG(
+#     dag_id="dr_maintenance_dag",
+#     start_date=datetime(2023, 1, 1),
+#     schedule_interval=None,
+#     catchup=False,
+#     default_args=default_args,
+#     description="Disaster recovery: create backup workspaces from primary deployments",
+# ) as dag:
+#
+#     get_deployments_task = PythonOperator(
+#         task_id="get_deployments",
+#         python_callable=get_deployments,
+#         op_kwargs={"mode": "source"},
+#     )
+#
+#     create_backup_workspaces_task = PythonOperator(
+#         task_id="create_backup_workspaces",
+#         python_callable=create_backup_workspaces,
+#         op_args=[XComArg(get_deployments_task)],
+#     )
+#
+#     create_backup_deployments_task = PythonOperator(
+#         task_id="create_backup_deployments",
+#         python_callable=create_backup_deployments,
+#     )
+#
+#     unhibernate_backup_deployments_task = PythonOperator(
+#         task_id="unhibernate_backup_deployments",
+#         python_callable=manage_backup_hibernation,
+#         op_kwargs={"deployment_set": "backup", "action": "unhibernate"},
+#     )
+#
+#     # sync_tokens_task = PythonOperator(
+#     #     task_id="sync_tokens_to_backup_workspaces",
+#     #     python_callable=sync_tokens_to_backups,
+#     # )
+#
+#     # hibernate_backup_deployments_task = PythonOperator(
+#     #     task_id="hibernate_backup_deployments",
+#     #     python_callable=manage_backup_hibernation,
+#     #     op_kwargs={"deployment_set": "backup", "action": "hibernate"},
+#     # )
+#
+#     # Set task dependencies
+#     # get_deployments_task >> create_backup_workspaces_task >> create_backup_deployments_task >> unhibernate_backup_deployments_task >> sync_tokens_task
